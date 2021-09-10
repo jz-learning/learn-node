3d5c209d66585b595ecb1c188d668cbd6ef0f403
James Zhang <jameszhanga@gmail.com>
2021-09-10 02:06:18 -0500

    Finish GET and POST functions

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+const { application } = require('express');
+
 const app = require('express')();
 const PORT = 8080;
 
 // Start our API, listen on port
 app.listen(PORT, () => console.log(`API is live on http://localhost:${PORT}`));
+
+// Add endpoint to API
+
+//! GET
+// Request  = incoming data,
+// Response = Data to send back
+app.get('/tshirt', (req, res) => {
+  // OK Request
+  res.status(200).send({
+    tshirt : '👕',
+    size   : 'large'
+  });
+});
+
+//! POST
+app.post('.tshirt/:id', (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({ Message: 'We need a logo' });
+  }
+
+  res.send({
+    tshirt : "👕 with yout $'{logo}"
+  });
+});
